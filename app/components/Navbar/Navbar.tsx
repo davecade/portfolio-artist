a444d591b15b8d0faf702eab199d050a611b0e7c
Dave <dave.cadelina@outlook.com>
2024-01-08 23:46:01 +1100

    navbar styles done

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-import Link from "next/link";
 import styles from "./Navbar.module.scss"; // Assume you have some styles defined
-import CustomLogo from "../CustomLogo/CustomLogo";
+import mySvg from "../../../public/logo-web-icon.svg";
+import Image from "next/image";
+import {
+	FaFacebook,
+	FaTwitter,
+	FaInstagram,
+	FaLinkedin,
+	FaPatreon,
+} from "react-icons/fa";
 
 const Navbar = () => {
 	return (
 		<nav className={styles.container}>
-			<CustomLogo />
+      <div className={styles.container_inner} >
+        <div className={styles.hamburger_container}>
+          <div className={styles.hamburger_line}></div>
+          <div className={styles.hamburger_line}></div>
+          <div className={styles.hamburger_line}></div>
+        </div>
+        <div className={styles.logo_container}>
+          <Image src={mySvg} alt="My SVG" width={40} height={40} />
+        </div>
+        <div className={styles.social_media_container}>
+          <FaPatreon className={styles.social_media} size={25} />
+          <FaLinkedin className={styles.social_media} size={25} />
+          <FaFacebook className={styles.social_media} size={25} />
+          <FaTwitter className={styles.social_media} size={25} />
+          <FaInstagram className={styles.social_media} size={25} />
+        </div>
+      </div>
 		</nav>
 	);
 };
