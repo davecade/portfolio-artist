3f6c97427065c9b83400498ec166fb433297474b
Dave <dave.cadelina@outlook.com>
2024-02-04 01:32:33 +1100

    fixed alignment for myservices header section

--- app/components/MyServices.tsx/index.tsx
+++ app/components/MyServices.tsx/index.tsx
@@ -9,16 +9,13 @@ const MyServices = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
-				<Heading text={"My Services"} size={"medium"} />
-				<div
-					style={{
-						width: "100%",
-						display: "flex",
-						justifyContent: "flex-end",
-						alignItems: "center",
-					}}
-				>
-					<ActionLink text={"Browse all services"} onClick={() => {}} />
+				<div className={styles.heading_action_link_container}>
+					<div className={styles.left_side}>
+						<Heading text={"My Services"} size={"medium"} />
+					</div>
+					<div className={styles.right_side}>
+						<ActionLink text={"Browse all services"} onClick={() => {}} />
+					</div>
 				</div>
 			</div>
 		</div>
