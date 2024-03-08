b6a021aa8b87897e8ab65ae39cc476c5e580b28b
Dave <dave.cadelina@outlook.com>
2024-03-09 02:01:50 +1100

    added menu open component

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -16,24 +16,28 @@ import HamburgerIcon from "../HamburgerIcon";
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
+	const handleMenuClick = () => {
+		setIsOpen((prev) => !prev);
+	};
+
 	return (
 		<nav className={styles.container}>
-			<div className={styles.container_inner}>
-				<HamburgerIcon
-					isActive={isOpen}
-					onClick={() => setIsOpen((prev) => !prev)}
-				/>
-				<div className={styles.logo_container}>
-					<Image src={mySvg} alt="My SVG" width={40} height={40} />
-				</div>
-				<div className={styles.social_media_container}>
-					<FaPatreon className={styles.social_media} size={25} />
-					<FaLinkedin className={styles.social_media} size={25} />
-					<FaFacebook className={styles.social_media} size={25} />
-					<FaTwitter className={styles.social_media} size={25} />
-					<FaInstagram className={styles.social_media} size={25} />
+			<div className={`${styles.closed_nav} ${isOpen ? styles.isOpen : ""}`}>
+				<div className={styles.closed_nav_inner}>
+					<HamburgerIcon isActive={isOpen} onClick={handleMenuClick} />
+					<div className={styles.logo_container}>
+						<Image src={mySvg} alt="My SVG" width={40} height={40} />
+					</div>
+					<div className={styles.social_media_container}>
+						<FaPatreon className={styles.social_media} size={25} />
+						<FaLinkedin className={styles.social_media} size={25} />
+						<FaFacebook className={styles.social_media} size={25} />
+						<FaTwitter className={styles.social_media} size={25} />
+						<FaInstagram className={styles.social_media} size={25} />
+					</div>
 				</div>
 			</div>
+			<div className={`${styles.menu} ${isOpen ? styles.menu_open : ""}`}></div>
 		</nav>
 	);
 };
